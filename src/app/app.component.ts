a1543cbc64449c984308e3b09790d7f329e0b957
Ricardo Mera Ciudad <ricardo.mera@bosonit.com>
2023-05-23 15:20:38 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ejercicio-1';
+
+  buttonText: string = 'Show ninja div';
+  showElement: boolean = false;
+
+  displayElement(): void {
+    this.showElement = !this.showElement;
+    this.showElement ? this.buttonText='Hide ninja div' : this.buttonText='Show ninja div';
+    console.log(this.showElement);
+  }
 }
+
+
